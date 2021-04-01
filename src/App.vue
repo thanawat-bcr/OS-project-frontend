48b5d3dcf009a0d929dcf231c6933e5aa2681b2d
thanawat-bcr <thanawat.bcr@gmail.com>
2021-04-01 19:53:37 +0700

    [CHANGE] host from NAT to bridge ip

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,8 @@ export default {
   data() {
     return {
       connection: {
-        host: "172.16.167.2",
+        host: "192.168.1.38",
+        // host: "172.16.167.2",
         // host: "127.0.0.1",
         port: 9001,
         endpoint: "",
